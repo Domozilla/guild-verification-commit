6c0c62f85a7b8c94036dca3e724f886522436e77
Domozilla <dawidek83b@gmail.com>
2026-03-05 21:15:15 +0100

    Fix memory leak in event handlers

--- helpers.ts
+++ helpers.ts
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-05T20:13:46.442Z
-Commit: Implement user profile customization
+Timestamp: 2026-03-05T20:15:04.064Z
+Commit: Fix memory leak in event handlers
