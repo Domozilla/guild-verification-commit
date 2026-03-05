25034ea1b1ee0aac78f9e82d792c2a4e1b230938
Domozilla <dawidek83b@gmail.com>
2026-03-05 21:13:55 +0100

    Resolve null pointer exception in user service

--- utils.js
+++ utils.js
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-05T20:13:39.471Z
-Commit: Fix memory leak in event handlers
+Timestamp: 2026-03-05T20:13:44.032Z
+Commit: Resolve null pointer exception in user service
