22b63a4fec94576cc3360f2412d389872332f79d
Domozilla <dawidek83b@gmail.com>
2026-03-05 21:16:03 +0100

    Resolve null pointer exception in user service

--- main.py
+++ main.py
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-05T20:14:15.136Z
-Commit: Add export functionality for reports
+Timestamp: 2026-03-05T20:15:52.183Z
+Commit: Resolve null pointer exception in user service
