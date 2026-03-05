264c93ce261be0181727b799a77b9123dc4b3a85
Domozilla <dawidek83b@gmail.com>
2026-03-05 21:13:57 +0100

    Implement user profile customization

--- helpers.ts
+++ helpers.ts
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-05T20:13:19.901Z
-Commit: Fix broken links in documentation
+Timestamp: 2026-03-05T20:13:46.442Z
+Commit: Implement user profile customization
