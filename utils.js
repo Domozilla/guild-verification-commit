37b2fe867f899938f8945e658e39828990bccd1f
Domozilla <dawidek83b@gmail.com>
2026-03-05 21:16:10 +0100

    Resolve timezone issues in date handling

--- utils.js
+++ utils.js
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-05T20:15:53.633Z
-Commit: Fix CSS styling bug on mobile devices
+Timestamp: 2026-03-05T20:15:58.772Z
+Commit: Resolve timezone issues in date handling
