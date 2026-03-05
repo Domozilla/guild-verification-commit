5edbf4185782aaba44bf0b18e6af6d405e0a2dc1
Domozilla <dawidek83b@gmail.com>
2026-03-05 21:17:25 +0100

    Add export functionality for reports

--- utils.js
+++ utils.js
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-05T20:17:08.468Z
-Commit: Resolve null pointer exception in user service
+Timestamp: 2026-03-05T20:17:14.432Z
+Commit: Add export functionality for reports
