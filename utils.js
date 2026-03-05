5e2836732a74bc5d7cfb0c549e6d92e045f9edcf
Domozilla <dawidek83b@gmail.com>
2026-03-05 21:17:19 +0100

    Resolve null pointer exception in user service

--- utils.js
+++ utils.js
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-05T20:17:00.523Z
-Commit: Add export functionality for reports
+Timestamp: 2026-03-05T20:17:08.468Z
+Commit: Resolve null pointer exception in user service
