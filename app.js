8b9e05ecf36c7baaa396cc67ab3e366a9d9756c8
Domozilla <dawidek83b@gmail.com>
2026-03-05 21:14:14 +0100

    Fix broken links in documentation

--- app.js
+++ app.js
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-05T20:13:48.969Z
-Commit: Fix race condition in async operations
+Timestamp: 2026-03-05T20:14:02.634Z
+Commit: Fix broken links in documentation
