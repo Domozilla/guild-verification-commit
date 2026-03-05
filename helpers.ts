9b163b45e2196e3eee0c9f02eb0ab0e089144517
Domozilla <dawidek83b@gmail.com>
2026-03-05 21:17:02 +0100

    Add export functionality for reports

--- helpers.ts
+++ helpers.ts
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-05T20:16:40.301Z
-Commit: Resolve null pointer exception in user service
+Timestamp: 2026-03-05T20:16:51.429Z
+Commit: Add export functionality for reports
