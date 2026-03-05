4bc1fc851bb5e9e794db1fe909e5033aa08bcd9f
Domozilla <dawidek83b@gmail.com>
2026-03-05 21:16:04 +0100

    Fix CSS styling bug on mobile devices

--- utils.js
+++ utils.js
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-05T20:15:49.610Z
-Commit: Introduce real-time notifications
+Timestamp: 2026-03-05T20:15:53.633Z
+Commit: Fix CSS styling bug on mobile devices
