8c4a957f5e1f5dd01da7b419f0b93e6e523648ae
Domozilla <dawidek83b@gmail.com>
2026-03-05 21:16:39 +0100

    Fix broken links in documentation

--- app.js
+++ app.js
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-05T20:16:21.992Z
-Commit: Fix CSS styling bug on mobile devices
+Timestamp: 2026-03-05T20:16:27.959Z
+Commit: Fix broken links in documentation
