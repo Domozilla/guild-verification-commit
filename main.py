596246d3a49ea4758781631f8c564b4a838573b5
Domozilla <dawidek83b@gmail.com>
2026-03-05 21:16:32 +0100

    Resolve timezone issues in date handling

--- main.py
+++ main.py
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-05T20:15:52.183Z
-Commit: Resolve null pointer exception in user service
+Timestamp: 2026-03-05T20:16:20.722Z
+Commit: Resolve timezone issues in date handling
