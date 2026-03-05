dce28e575aebaa86ba7935f2fc861e0c8c773df8
Domozilla <dawidek83b@gmail.com>
2026-03-05 21:17:11 +0100

    Add export functionality for reports

--- utils.js
+++ utils.js
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-05T20:16:29.179Z
-Commit: Implement user profile customization
+Timestamp: 2026-03-05T20:17:00.523Z
+Commit: Add export functionality for reports
